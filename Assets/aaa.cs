d3f452d11ec83a30227ade94170f47eca3a4c5ed
VanPhamNe <119727678+VanPhamNe@users.noreply.github.com>
2025-05-11 19:12:18 +0700

    Update

--- Assets/aaa.cs
+++ Assets/aaa.cs
@@ -7,7 +7,7 @@ public class aaa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ahhahaha");
     }
 
     // Update is called once per frame
